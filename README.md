df77f18c6fcb649913b9d3475fc3a37bf44d45af
Alfons Bengtegård <alfonsbengtegard@gmail.com>
2023-11-06 12:12:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# prpeek
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun start --repo https://github.com/<username>/<repository>
+```
+
+To buld:
+```bash
+bun compile
+```
+
+This project was created using `bun init` in bun v1.0.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,92 @@
+import terminalLink from "terminal-link";
+import chalk from "chalk";
+import boxen from "boxen";
+import { Command } from "commander";
+const program = new Command();
+
+program
+  .description("Show status of relevant pull requests live")
+  .requiredOption(
+    "-r, --repo <repo>",
+    "Repository url to target: [HOST/]OWNER/REPO"
+  )
+  .option("-i, --interval <interval>", "Update interval in seconds", "5");
+
+program.parse();
+
+const { repo, interval } = program.opts();
+
+const intervalAsMillis = Number(interval * 1000);
+
+if (isNaN(intervalAsMillis)) {
+  program.error('Interval must be a number');
+}
+
+const runGhCommand = (repoUrl: string) => {
+  const env = { ...process.env, CLICOLOR_FORCE: "1", GH_PAGER: "" };
+  const ghPrCommand = ["gh", "pr", "status", "--repo", repoUrl];
+
+  return Bun.spawn(ghPrCommand, {
+    env,
+  });
+};
+
+const getPrLink = (prNumber: string) => {
+  return `${repo}/pull/${prNumber}`;
+};
+
+const formatOutput = (text: string) => {
+  const formattedWithLinks = text.replace(
+    /^(.*#(\d+).*)$/gm,
+    (match, line, prNumber) => {
+      const link = terminalLink(line.trimStart(), getPrLink(prNumber));
+      return `  ${link}`;
+    }
+  );
+
+  const lines = formattedWithLinks.split("\n");
+  const title = lines[1];
+
+  lines.splice(0, 3);
+  const body = lines.join("\n");
+
+  return { title, body };
+};
+
+const getDate = () => {
+  const date = new Date();
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    timeStyle: "medium",
+    hour12: false,
+  };
+
+  const formattedDate = new Intl.DateTimeFormat("se-sv", dateOptions).format(
+    date
+  );
+  const formattedTime = new Intl.DateTimeFormat("se-sv", timeOptions).format(
+    date
+  );
+
+  return `${formattedDate} ${formattedTime}`;
+};
+
+while (true) {
+  const ghCommand = runGhCommand(repo);
+  const ghCommandOuput = await new Response(ghCommand.stdout).text();
+  const date = chalk.dim(`Last updated: ${getDate()}`);
+
+  console.clear();
+
+  const { body, title } = formatOutput(ghCommandOuput);
+  const output = `${body}\n${date}`;
+
+  console.log(boxen(output, { padding: 1, dimBorder: true, title: title }));
+
+  await Bun.sleep(intervalAsMillis);
+}
