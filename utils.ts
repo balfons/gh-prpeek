58c523b2f8a80f79aee052167515b28c7ca86195
Alfons Bengtegård <alfonsbengtegard@gmail.com>
2024-01-12 18:02:55 +0100

    refactor utils

---
+++ utils.ts
@@ -0,0 +1,16 @@
+export const clearScreen = () => {
+  process.stdout.write("\u001b[3J\u001b[1J"); // Prevent scrollback
+  console.clear();
+};
+
+export const enableAlternateBuffer = () => {
+  process.stdout.write("\u001B[?1049h"); // Enable alternative buffer
+};
+
+export const disableAlternateBuffer = () => {
+  process.stdout.write("\u001B[?1049l"); // Disable alternative buffer
+};
+
+export const commaSeparatedList = (value: string): string[] => {
+  return value.toLowerCase().split(",");
+};
