b38acd1c83700558305b3c35a8d26fa698338280
Alfons Bengtegård <alfonsbengtegard@gmail.com>
2024-09-20 07:46:04 +0200

    Fix compile targets

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 bun install
-FILE_PATH="dist/gh-prpeek_$(echo $1)_darwin-amd64" bun compile
+FILE_PATH="gh-prpeek_$(echo $1)_linux-x64" TARGET="bun-linux-x64" bun release
+FILE_PATH="gh-prpeek_$(echo $1)_linux-arm64" TARGET="bun-linux-arm64" bun release
+FILE_PATH="gh-prpeek_$(echo $1)_windows-x64" TARGET="bun-windows-x64" bun release
+FILE_PATH="gh-prpeek_$(echo $1)_darwin-x64" TARGET="bun-darwin-x64" bun release
+FILE_PATH="gh-prpeek_$(echo $1)_darwin-arm64" TARGET="bun-darwin-arm64" bun release
 exit 0
