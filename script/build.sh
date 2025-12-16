9db48e33b84302f32636e8139c079ee435e30c26
Alfons Bengtegård <alfonsbengtegard@gmail.com>
2025-12-16 12:40:41 +0100

    explain weird build script

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-# bun install
-
-# FILE_PATH="gh-prpeek_$(echo $1)_linux-x64" TARGET="bun-linux-x64" bun release
-# FILE_PATH="gh-prpeek_$(echo $1)_linux-arm64" TARGET="bun-linux-arm64" bun release
-# FILE_PATH="gh-prpeek_$(echo $1)_windows-x64" TARGET="bun-windows-x64" bun release
-# FILE_PATH="gh-prpeek_$(echo $1)_darwin-x64" TARGET="bun-darwin-x64" bun release
-# FILE_PATH="gh-prpeek_$(echo $1)_darwin-arm64" TARGET="bun-darwin-arm64" bun release
+# The build is done in the GitHub Actions workflow using bun build command.
+# This script is a placeholder to satisfy the build_script_override requirement when building extensions not written in Go.
 exit 0
