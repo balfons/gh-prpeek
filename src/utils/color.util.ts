e9b570f7b8ab5d9c5da1aa0976a10d91cc11bb4e
Alfons Bengtegård <alfonsbengtegard@gmail.com>
2026-04-20 12:41:10 +0200

    Evaluate color keys before loop instead of on every iteration

--- src/utils/color.util.ts
+++ src/utils/color.util.ts
@@ -194,8 +194,10 @@ export const setTerminalColorsFromTheme = async (renderer: CliRenderer) => {
     defaultBackground: "",
   };
 
+  const colorKeys = Object.keys(terminalThemeHexColors);
+
   palette.palette.forEach((color, index) => {
-    const colorName = Object.keys(terminalThemeHexColors)[index];
+    const colorName = colorKeys[index];
     (terminalThemeHexColors as any)[colorName] = color || "";
   });
   terminalThemeHexColors.defaultBackground = palette.defaultBackground || "";
@@ -224,9 +226,9 @@ export const getHexColor = (colorName: keyof TerminalThemeColors): string => {
 };
 
 export const colorIsDarkSimple = (bgColor: string): boolean => {
-  let color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  let color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
   let r = parseInt(color.substring(0, 2), 16); // hexToR
   let g = parseInt(color.substring(2, 4), 16); // hexToG
   let b = parseInt(color.substring(4, 6), 16); // hexToB
-  return ((r * 0.299) + (g * 0.587) + (b * 0.114)) <= 186;
-}
+  return r * 0.299 + g * 0.587 + b * 0.114 <= 186;
+};
